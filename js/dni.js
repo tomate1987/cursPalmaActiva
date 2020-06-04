21fa794eb2d05a52633a60ec6c85ac7bc5cb8df2
jaume <jmoya@imas.conselldemallorca.net>
2020-06-04 11:56:14 +0200

    dniapp

---
+++ js/dni.js
@@ -0,0 +1,49 @@
+//funcio comprobar dni
+function comprobarPolicia(dni){
+
+//recuperam el nom i el dni del formulari
+    var nom = document.getElementById("nom").value;
+    var numDNI = document.getElementById("dni").value;
+
+    //pasam el numDni a la funcio validar per mirar si es dni mab la lletra es correcte.
+ var comprobar = validar(numDNI);
+
+ if (comprobar) {
+
+    alert ('El Dni de ' +nom+ ' es correcte');
+
+  
+
+ }else {
+    alert ('El Dni de ' +nom+ ' NO es correcte');
+ 
+
+ }
+
+}
+
+// funcio validar numDni.
+function validar(numDNI){
+
+    //llevam la letra.
+    var numDNISinLetra = numDNI.substring(0, 8);
+
+    //lletres dni
+    var cadena="TRWAGMYFPDXBNJZSQVHLCKET";
+    //cercam quina posicio ens torna per despres mirarla amb la lletra
+    var posicion = numDNISinLetra%23;
+    var letra = cadena.substring(posicion,posicion+1);
+
+    //concatanam la lletra amb el numero de dni.
+    var dni_complet =String(numDNISinLetra + letra);
+
+    //comprobam el dni pasat per formulario amb el dni mab la seva lletra correcta.
+    if(dni_complet ===numDNI){
+        return true;
+    }else {
+        return false;
+    }
+
+}
+
+   
